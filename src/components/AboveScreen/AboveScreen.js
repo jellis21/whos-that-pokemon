5792196be91a06a7d74107f605d60b2be4a43650
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-12-28 13:18:24 -0500

    Starts building AboveScreen

--- src/components/AboveScreen/AboveScreen.js
+++ src/components/AboveScreen/AboveScreen.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import './AboveScreen.css'
+import React from "react";
+import "./AboveScreen.css";
 
 function AboveScreen() {
   return (
     <div className="above-screen">
-      hello
+      <div className="above-screen__section">
+        <div className="above-screen__section__light-container">
+          <div className="above-screen__section__light"></div>
+        </div>
+      </div>
+      <div className="above-screen__section">
+        <div className="above-screen__section__button__container">
+          <div className="above-screen__section__button"></div>
+          <div className="above-screen__section__button"></div>
+        </div>
+      <svg height="70" width="180">
+        <line
+          x1="0"
+          y1="69"
+          x2="168"
+          y2="0"
+          style={{stroke: "black", strokeWidth: 2}}
+        />
+      </svg>
+      </div>
+      <div className="above-screen__section">
+        <div className="above-screen__section__line"></div>
+        <div className="above-screen__section__line"></div>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default AboveScreen
+export default AboveScreen;
