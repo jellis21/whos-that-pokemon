5b49abb7d04d9e9ca1e6aff243ff9951c6e1eb37
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-12-28 17:52:14 -0500

    Styles Search and Home, adds Google font

--- src/routes/Landing/Landing.js
+++ src/routes/Landing/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import pokemonImage from "../../images/who-pokemon.jpg";
 import './Landing.css';
 
 function Landing() {
   return (
     <div className="landing">
-      I am landing
+      <img className="landing__image" src={pokemonImage} alt="who's that pokemon?" />
     </div>
   )
 }
--- src/routes/Search/Search.js
+++ src/routes/Search/Search.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 import { useEffect, useState } from "react";
-import './Search.css';
+import "./Search.css";
 
 function Search() {
-
   const [pokemon, setPokemon] = useState(null);
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon-species/squirtle/")
+    fetch("https://pokeapi.co/api/v2/pokemon-species/mew/")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -16,7 +15,7 @@ function Search() {
 
   const [pokemon1, setPokemon1] = useState(null);
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/squirtle/")
+    fetch("https://pokeapi.co/api/v2/pokemon/mew/")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -29,14 +28,18 @@ function Search() {
   }
   return (
     <div className="search">
-                <div className="search__content">
-            <h3>{pokemon.name}</h3>
-            <p>No. {pokemon.id}</p>
-            <img src={pokemon1.sprites.front_default} alt={pokemon.name} />
-            <p>{pokemon.flavor_text_entries[0].flavor_text}</p>
-          </div>
+      <div className="search__content">
+        <div className="a">
+          <h3>{pokemon.name}</h3>
+          <p>No. {pokemon.id}</p>
+        </div>
+        <div className="b">
+        <img src={pokemon1.sprites.front_default} alt={pokemon.name} />
+        <p>{pokemon.flavor_text_entries[0].flavor_text}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
